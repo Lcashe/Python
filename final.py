4cb0ffe14e0fbc287bb2913e12e2631d0411c305
Lcashe <megaasda@gmail.com>
2021-01-16 10:32:56 +0200

    Pretty table

--- final.py
+++ final.py
@@ -2,27 +2,18 @@ import re, requests
 from collections import Counter
 from prettytable import PrettyTable
 
-url_input = input("Enter url: ")
+url_input = str(input("Enter url: "))
 
-url_checked = re.findall(r'https?://\S+', url_input)[0] # берем первый элемент
-
-if url_input != url_checked: # проверка валидности ссылки
-    print("Entered url is invalid")
-else:
-    pass
+url_checked = re.findall(r'(http[s]?://)?\S+', url_input)[0] # берем первый элемент
 
 response = requests.get(str(url_checked)) # запрос на введенную ссылку
 
-result = re.findall( r"\"(?:http[s]?://)([^:/\s\"]+)/?[^\"]*\"", response.text) # фильтрация ссылок
+result = re.findall( r"\"(?:http[s]?://)?([^:/\s\"]+)/?[^\"]*\"", response.text) # фильтрация ссылок
 
 result.sort() # sorting by alphabet 
 
 # link - https://stackoverflow.com/
 
-def count_words(List):
-    for link, counter in Counter(List).most_common():
-        print(f"{link} matches {counter} time(s)")
-
-count_words(result)
-
-counter_list = list(Counter(result).most_common())
+pt = PrettyTable(field_names = ["word", "counter"])
+pt.add_rows(list(Counter(result).most_common()))
+print(pt)
