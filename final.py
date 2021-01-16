94629d6dd4c522e32dccd717488e06cc0fb0747a
Lcashe <megaasda@gmail.com>
2021-01-16 10:27:26 +0200

    Added input function

--- final.py
+++ final.py
@@ -1,6 +1,21 @@
-import requests
-import re
+import re, requests
+from collections import Counter
 
-response = requests.get("https://www.google.com/")
-result = re.findall(r'^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$', response.text)
-print(result)
+url_input = input("Enter url: ")
+
+url_checked = re.findall(r'https?://\S+', url_input)[0] # берем первый элемент
+
+
+if url_input != url_checked: # проверка валидности ссылки
+    print("Entered url is invalid")
+else:
+    pass
+
+response = requests.get(str(url_checked)) # запрос на введенную ссылку
+result = re.findall(r'/[A-Za-z.]+/', response.text) # фильтрация ссылок
+
+def count_words(List): # функция подсчета ссылок
+    for word, counter in Counter(List).items():
+        print(f"{word} matches {counter} times")
+
+count_words(result)
